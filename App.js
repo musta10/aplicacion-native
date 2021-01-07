9f3ab865e265e86423a92df3eda99aa4cc31a62e
musta10 <mustafaenfadal@gmail.com>
2021-01-07 21:04:17 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to details Screen"
+        onPress={() => navigation.navigate("Details")}
+      />
     </View>
   );
-}
+};
+const DetailsScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <Button
+        title="pasar las pantallas siguiente..."
+        onPress={() => navigation.push("Details")}
+      />
+        <Button
+        title="volver al Home"
+        onPress={() => navigation.navigate("Home")}
+      />
+        <Button
+        title="volver atras"
+        onPress={() => navigation.goBack()}
+      />
+        <Button
+        title="la primera pantalla"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
